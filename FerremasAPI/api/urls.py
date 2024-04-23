4a05c783b6f032cd6920dd31f0d53e6a7a1dad1f
Abel <abelx3678@gmail.com>
2024-04-23 17:57:55 -0400

    Pasados las vistas y url de apis

--- FerremasAPI/api/urls.py
+++ FerremasAPI/api/urls.py
@@ -1,3 +1,25 @@
 from django.urls import path
+from . import views
+from .views import UsuarioPorCorreoApi, DetallePorIdApi, VentaPorIdApi
 
-urlpatterns=[]
+urlpatterns=[
+    path('api/categorias/', views.listaCategoriasApi.as_view(), name='api-categorias'),
+    path('api/usuarios/', views.listaUsuariosApi.as_view(), name='api-usuarios'),
+    path('api/productos/', views.listaProductosApi.as_view(), name='api-productos'),
+    path('api/ventas/', views.listaVentasApi.as_view(), name='api-ventas'),
+    path('api/detalles/', views.listaDetallesApi.as_view(), name='api-detalles'),
+    path('api/compras/', views.listaComprasApi.as_view(), name='api-compras'),
+    path('api/consultas/', views.listaConsultasApi.as_view(), name='api-consultas'),
+    path('api/producto/', views.productoApi.as_view(), name='api-producto'),
+    path('api/detallesProducto/', views.listaDetallesProductoApi.as_view(), name='api-detalles-producto'),
+    path('api/usuario/<str:correo>/', UsuarioPorCorreoApi.as_view(), name='usuario-por-correo'),
+    path('api/filtrar-carrito/', views.FiltrarCarritoAPI.as_view(), name='filtrar-carrito'),
+    path('api/detalles-carrito/', views.DetallesCarritoAPI.as_view(), name='detalles-carrito'),
+    path('api/detalles-id-carrito/', views.DetallesCarritoPorIdAPI.as_view(), name='detalles-id-carrito'),
+    path('api/delete-detalle/', views.DeleteDetallePorIdApi.as_view(), name='delete-detalle'),
+    path('api/detalles-buscar-carrito/', views.DetallesBuscarCarritoAPI.as_view(), name='detalles-buscar-carrito'),
+    path('api/detalle/<str:id_detalle>/', DetallePorIdApi.as_view(), name='detalle-por-id'),
+    path('api/venta/<str:id_venta>/', VentaPorIdApi.as_view(), name='venta-por-id'),
+    path('api/crear-detalle/', views.CrearDetalleAPI.as_view(), name='crear-detalle'),
+    path('api/crear-venta/', views.CrearVentaAPI.as_view(), name='crear-venta'),
+]
--- FerremasAPI/api/views.py
+++ FerremasAPI/api/views.py
@@ -1,3 +1,150 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import categoriaSerializer, usuarioSerializer, productoSerializer, consultaSerializer, ventaSerializer, detalleSerializer, detalleCompradoSerializer, detalleConProductoSerializer, transaccionSerializer
+from .models import Categoria, Consulta, Usuario, Producto, Venta, Detalle,  Detalle_comprado, Transaccion
+
 
 # Create your views here.
+class listaCategoriasApi(generics.ListAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = categoriaSerializer
+
+class listaUsuariosApi(generics.ListAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = usuarioSerializer
+
+class UsuarioPorCorreoApi(generics.RetrieveAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = usuarioSerializer
+    lookup_field = 'correo'
+
+class listaProductosApi(generics.ListAPIView):
+    serializer_class = productoSerializer
+
+    def get_queryset(self):
+        queryset = Producto.objects.all()
+        categoria = self.request.query_params.get('categoria')
+        if categoria is not None:
+            queryset = queryset.filter(categoria=categoria)
+        return queryset
+    
+class productoApi(generics.ListAPIView):
+    serializer_class = productoSerializer
+
+    def get_queryset(self):
+        queryset = Producto.objects.all()
+        codigo = self.request.query_params.get('cod_prod')
+        if codigo is not None:
+            queryset = queryset.filter(cod_prod=codigo)
+        return queryset
+
+class listaVentasApi(generics.ListAPIView):
+    queryset = Venta.objects.all()
+    serializer_class = ventaSerializer
+
+class listaConsultasApi(generics.ListAPIView):
+    queryset = Consulta.objects.all()
+    serializer_class = consultaSerializer
+
+class listaDetallesApi(generics.ListAPIView):
+    queryset = Detalle.objects.all()
+    serializer_class = detalleSerializer
+
+class DetallePorIdApi(generics.RetrieveUpdateAPIView):
+    queryset = Detalle.objects.all()
+    serializer_class = detalleSerializer
+    lookup_field = 'id_detalle'
+
+class DeleteDetallePorIdApi(APIView):
+    def delete(self, request):
+        id_detalle = request.GET.get('id_detalle')
+
+        try:
+            detalle = Detalle.objects.get(id_detalle=id_detalle)
+        except Detalle.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        detalle.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class listaDetallesProductoApi(generics.ListAPIView):
+    queryset = Detalle.objects.all()
+    serializer_class = detalleConProductoSerializer
+
+class listaComprasApi(generics.ListAPIView):
+    queryset = Detalle_comprado.objects.all()
+    serializer_class = detalleCompradoSerializer
+
+class FiltrarCarritoAPI(APIView):
+    def get(self, request):
+        # Obtener los parámetros de consulta de la URL
+        usuario = request.GET.get('usuario')
+        estado = request.GET.get('estado')
+
+        # Filtrar usuarios basados en los parámetros
+        carrito = Venta.objects.all()
+        if carrito:
+            carrito = carrito.filter(usuario=usuario)
+        if estado:
+            carrito = carrito.filter(estado=estado)
+
+        # Serializar los resultados y devolver la respuesta
+        serializer = ventaSerializer(carrito, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class DetallesCarritoAPI(APIView):
+    def get(self, request):
+        # Obtener el ID de la venta desde los parámetros de la URL
+        id_venta = request.GET.get('venta')
+
+        # Buscar todos los detalles del carrito basados en el ID de la venta
+        detalles_carrito = Detalle.objects.filter(venta=id_venta)
+
+        # Serializar los resultados y devolver la respuesta
+        serializer = detalleConProductoSerializer(detalles_carrito, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class DetallesCarritoPorIdAPI(APIView):
+    def get(self, request):
+        id_detalle = request.GET.get('id_detalle')
+
+        detalles_carrito = Detalle.objects.filter(id_detalle=id_detalle)
+
+        serializer = detalleConProductoSerializer(detalles_carrito, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class VentaPorIdApi(generics.RetrieveUpdateAPIView):
+    queryset = Venta.objects.all()
+    serializer_class =ventaSerializer
+    lookup_field = 'id_venta'
+
+class DetallesBuscarCarritoAPI(APIView):
+    def get(self, request):
+        # Obtener el ID de la venta y el código del producto desde los parámetros de la URL
+        id_venta = request.GET.get('venta')
+        cod_producto = request.GET.get('producto')
+
+        # Filtrar los detalles del carrito basados en el ID de la venta y el código del producto
+        detalles_carrito = Detalle.objects.filter(venta=id_venta, producto__cod_prod=cod_producto)
+
+        # Serializar los resultados y devolver la respuesta
+        serializer = detalleConProductoSerializer(detalles_carrito, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CrearDetalleAPI(APIView):
+    def post(self, request):
+        serializer = detalleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CrearVentaAPI(APIView):
+    def post(self, request):
+        serializer = ventaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
